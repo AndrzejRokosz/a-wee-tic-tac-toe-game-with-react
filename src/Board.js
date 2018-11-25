3755c5aa2fcc70c16396791bdf92e2985d5fe781
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-25 18:10:22 +0100

    game working now

--- src/Board.js
+++ src/Board.js
@@ -3,31 +3,61 @@ import Square from './Square'
 
 
 class Board extends React.Component {
-    state={
-        squares:Array(9).fill(null),
-        xIsNext:true
+    state = {
+        squares: Array(9).fill(null),
+        xIsNext: true
     }
 
-    handleClick(i){
-        const squares= this.state.squares.slice()
-        squares[i]=this.state.xIsNext ? 'X':'O'
+    handleClick(i) {
+        const squares = this.state.squares.slice()
+        if (this.calculateWinner(squares) || squares[i]) {
+            return
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
         this.setState({
-            squares:squares,
-            xIsNext:!this.state.xIsNext
+            squares: squares,
+            xIsNext: !this.state.xIsNext
         })
+    }
+
+    calculateWinner(squares) {
+        const winningLines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for (let i = 0; i < winningLines.length; i++) {
+            const [a, b, c] = winningLines[i]
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a]
+            }
+        }
+        return null
     }
 
     renderSquare(i) {
         return (
-        <Square 
-        value ={this.state.squares[i]}
-        onClick={()=>this.handleClick(i)}
-        />)
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />)
     }
 
     render() {
-        const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
-
+        const winner = this.calculateWinner(this.state.squares)
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner
+        } else {
+            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+        }
         return (
             <div>
                 <div className="status">{status}</div>
